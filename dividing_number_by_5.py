78152d94b0a2a758895f533a3bcb04c12935a43d
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 09:17:42 +0700

    first

--- dividing_number_by_5.py
+++ dividing_number_by_5.py
@@ -2,7 +2,12 @@
 #numbers and print only those numbers which are divisible by 5
 
 # Defining function for dividing numbers by divisible by 5
+def displaying_numbers_divisible_by_5(input_list):
+ 
 # Iterate through the list
+    for number in input_list:
+        if number % 5 == 0
+        print(number)
 # Check if the number is divisible by 5
 
 # Test case
