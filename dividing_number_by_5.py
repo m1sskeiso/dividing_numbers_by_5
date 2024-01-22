53f13bbe920abb8291a77e0a509d74f46d85eb2e
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 09:14:39 +0700

    pseudocode

---
+++ dividing_number_by_5.py
@@ -0,0 +1,8 @@
+# Exercise 6: Display numbers divisible by 5 from a list Iterate the given list of 
+#numbers and print only those numbers which are divisible by 5
+
+# Defining function for dividing numbers by divisible by 5
+# Iterate through the list
+# Check if the number is divisible by 5
+
+# Test case
